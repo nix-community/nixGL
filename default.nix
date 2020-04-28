7aa1cce7c6e844cbc2552e47f251c8f2d38ab8f1
zimbatm <zimbatm@zimbatm.com>
2020-04-28 11:35:04 +0200

    allow to pass a nvidiaVersionFile
    This allows to better control which driver is being loaded.

--- default.nix
+++ default.nix
@@ -4,6 +4,11 @@
   # Hash of the Nvidia driver .run file. null is fine, but fixing a value here
   # will be more reproducible and more efficient.
   nvidiaHash ? null,
+  # Alternatively, you can pass a path that points to a nvidia version file
+  # and let nixGL extract the version from it. That file must be a copy of
+  # /proc/driver/nvidia/version. Nix doesn't like zero-sized files (see
+  # https://github.com/NixOS/nix/issues/3539 ).
+  nvidiaVersionFile ? null,
   # Enable 32 bits driver
   # This is one by default, you can switch it to off if you want to reduce a
   # bit the size of nixGL closure.
@@ -15,5 +20,10 @@
   }
 }:
 pkgs.callPackage ./nixGL.nix {
-  inherit nvidiaVersion nvidiaHash enable32bits;
+  inherit
+    nvidiaVersion
+    nvidiaVersionFile
+    nvidiaHash
+    enable32bits
+    ;
 }
