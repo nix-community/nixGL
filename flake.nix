69e5e3e2161ccb72f5882cc0cabbbb9db693ec63
Bernardo Meurer <bernardo@meurer.org>
2022-01-31 09:57:29 -0800

    refactor(flake): only expose overlay

--- flake.nix
+++ flake.nix
@@ -1,24 +1,8 @@
 {
   description = "A wrapper tool for nix OpenGL applications";
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self }: {
+    overlay = _: prev: {
+      nixgl = import ./default.nix { pkgs = prev; };
+    };
   };
-  outputs = { self, flake-utils, nixpkgs }:
-    {
-      overlay = _: prev: {
-        nixgl = import ./default.nix { pkgs = prev; };
-      };
-    }
-    // flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-        overlays = [ self.overlay ];
-      };
-    in
-    {
-      packages.nixGLIntel = pkgs.nixgl.nixGLIntel;
-      packages.nixVulkanIntel = pkgs.nixgl.nixVulkanIntel;
-    });
 }
