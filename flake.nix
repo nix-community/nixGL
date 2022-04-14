4a3093f9959927d08a9b4579b94cc4b4e3a8075f
Paul Grandperrin <paul.grandperrin@gmail.com>
2022-04-14 21:42:56 +0200

    add defaultPackage target to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,14 @@
 {
   description = "A wrapper tool for nix OpenGL applications";
-  outputs = { self }: {
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
+
+  outputs = { self, nixpkgs }: {
     overlay = final: _: {
       nixgl = import ./default.nix { pkgs = final; };
     };
+
+    # makes it easy to use "nix run nixGL --impure -- program"
+    defaultPackage.x86_64-linux = (import ./default.nix { pkgs = nixpkgs.legacyPackages.x86_64-linux; }).auto.nixGLDefault;
   };
 }
